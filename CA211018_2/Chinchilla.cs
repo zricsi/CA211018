aa88fad83d28f30c73ae92912fb2c8169f948c2a
Richárd Zloch <zloch.richard@verebelyszki.hu>
2021-10-18 14:50:57 +0200

    Add project files.

---
+++ CA211018_2/Chinchilla.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA211018_2
+{
+    class Chinchilla
+    {
+        public string Nev { get; set; }
+        public DateTime Szul { get; set; }
+        public int Suly { get; set; }
+        public bool Simi { get; set; }
+        public double Kor => (DateTime.Now - Szul).TotalDays / 365.25;
+        public Chinchilla(string r)
+        {
+            var t = r.Split(';');
+            Nev = t[0];
+            Szul = DateTime.Parse(t[1]);
+            Suly = int.Parse(t[2]);
+            Simi = t[3] == "I";
+        }
+    }
+}
---
+++ CA211018_2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA211018_2
+{
+    class Program
+    {
+        static List<Chinchilla>  csincsillak = new List<Chinchilla>();
+        static void Main(string[] args)
+        {
+            Beolvasas();
+            Console.WriteLine($"2. feladat: Összesen {csincsillak.Count} csincsilla van.");
+            var szereti = csincsillak.Count(x => x.Simi);
+            Console.WriteLine("3. feladat: {0:0.00}%-a szereti, ha simogatják",(float)szereti / csincsillak.Count * 100);
+            var oscs = csincsillak
+                .Where(x => x.Kor > 8 && x.Suly < 360)
+                .FirstOrDefault();
+            Console.WriteLine($"4. feladat:\n{(oscs is null ? "Nincs ilyen csincsilla" : $"{oscs.Nev} {Math.Floor(oscs.Kor)} éves és {oscs.Suly} kg.")}");
+            var rend = csincsillak.OrderByDescending(x => x.Suly).ToList();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("{0, -12} {1} g",rend[i].Nev, rend[i].Suly);
+            }
+            Console.WriteLine("6. feladat:");
+            csincsillak
+            .GroupBy(x => x.Szul.Year)
+            .ToList()
+            .ForEach(x => Console.WriteLine($"{x.Key}: {x.Count()}"));
+
+            
+
+            Console.ReadKey(true);
+        }
+
+        private static void Beolvasas()
+        {
+            using (var sr = new StreamReader(@"..\..\res\chi.txt", Encoding.UTF8))
+            {
+                while (!sr.EndOfStream) csincsillak.Add(new Chinchilla(sr.ReadLine()));
+            }
+        }
+    }
+}
